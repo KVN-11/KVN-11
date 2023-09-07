bb5e07afae712edb7d075f73fd8b2ea0b48653fc
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2023-09-06 22:25:54 -0400

    api added

---
+++ app.js
@@ -0,0 +1,17 @@
+const apiKey = "6c6ab19ea027118c671eaacb63d9d1c8";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin";
+
+window.addEventListener("load", checkWeather);
+
+async function checkWeather() {
+    const response = await fetch(apiURL + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".speed").innerHTML = data.wind.speed;
+
+}
