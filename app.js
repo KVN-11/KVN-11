62513e01f4dd655bb1ac6965a067b2aa5c638d4b
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2023-09-08 23:37:41 -0400

    Search feature added

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const apiKey = "6c6ab19ea027118c671eaacb63d9d1c8";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-window.addEventListener("load", checkWeather);
+// window.addEventListener("load", checkWeather);
+
+const searchbutton = document.querySelector("#searchbtn");
+
+
+searchbutton.addEventListener("click", ()=>{
+    checkWeather();
+})
 
 async function checkWeather() {
-    const response = await fetch(apiURL + `&appid=${apiKey}`);
+    var city = document.querySelector("#city").value;
+    console.log(city);
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "&#176;C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".speed").innerHTML = data.wind.speed;
-
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&#176;C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".speed").innerHTML = data.wind.speed + "km/hr";
 }
